affe33598c50954486bc273a6d511a42bf3ae7fa
Anvitha-shetty03 <anvithaanvithashetty@gmail.com>
2024-12-06 23:01:37 +0530

    Update reg.php

--- project/reg.php
+++ project/reg.php
@@ -8,6 +8,21 @@ $address=$_POST['address'];
 $image=$_FILES['photo']['name'];
 $tmp_name=$_FILES['photo']['tmp_name'];
 $role=$_POST['role'];
+if(strlen($mobile)!=10)
+{
+	echo'<script>
+	     alert("number must be 10 digits");
+		 window.location="reg.html";
+		 </script>';
+
+
+}
+if(is_numeric($name)){
+	echo'<script>
+	     alert("name must be characters");
+		 window.location="reg.html";
+		 </script>';
+}
 if($password==$cpassword){
 	move_uploaded_file($tmp_name,"$image");
 	$insert=mysqli_query($connect,"INSERT INTO login(name,mobile,address,password,role,photo,status,votes)VALUES('$name',$mobile,'$address','$password','$role','$image',0,0)");
